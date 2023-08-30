64e96c1c0c815fbb886321e13e7d482911810451
Salief Lewis <37086973+salieflewis@users.noreply.github.com>
2023-08-29 23:18:13 -0400

    Initial commit

---
+++ src/components/client/Header.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import { ConnectKitButton } from 'connectkit'
+import { Caisson, Deploy } from '../server'
+
+export function Header() {
+  return (
+    <div className="flex justify-between items-center p-4">
+      <Caisson />
+      <div className="flex items-center gap-8">
+        <Deploy />
+        <ConnectKitButton />
+      </div>
+    </div>
+  )
+}
